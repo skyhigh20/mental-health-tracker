4879b53cef85aaee002c0e03e9bc921ed9fd9062
skyhigh20 <nadhifryanalvaro@gmail.com>
2024-09-11 15:46:12 +0700

    Tutorial 2 selesai

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mental_health_tracker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'], # Tambahkan konten baris ini
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,3 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://https://pbp.cs.ui.ac.id/web/project/nadhif.ryan/mentalhealthtracker", "https://https://pbp.cs.ui.ac.id/web/project/nadhif.ryan/mentalhealthtracker"]
